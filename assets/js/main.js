732c881d850c3867b762c324de45e40d0dae61a8
usernamejahirrayhan <jahirrayhan6569@gmail.com>
2021-12-13 21:01:10 +0600

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -200,7 +200,7 @@ $(document).ready(function(){
   $('.accordian_header').click(function(){
     $(this).parent().siblings().find('.accordian_body').slideUp();
     $(this).parent().siblings().find('.icon_remove').removeClass('icon_remove');
-    $(this).next('.accordian_body').slideToggle('slow');
+    $(this).next('.accordian_body').slideToggle('fast');
     $(this).find('span').toggleClass('icon_remove');
   });
 });
